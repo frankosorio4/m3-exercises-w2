9b8bad78374c5c2328c3c858a94c06489e10ac35
Frank Osorio <frankosorio4@gmail.com>
2024-09-07 15:45:50 -0300

    feat: migration to add permission fiel added

---
+++ src/database/migrations/20240907000732-adicionando_campo_permissao_usuario.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('usuarios', 'permissao', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('usuarios', 'permission');
+  }
+};
