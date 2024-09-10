32c200aa682b097062dae39381be32c75adf068f
Frank Osorio <frankosorio4@gmail.com>
2024-09-09 22:03:46 -0300

    feat: seeder usuario added

--- src/database/seeders/20240909183800-demo_usuarios.js
+++ src/database/seeders/20240909183800-demo_usuarios.js
@@ -2,23 +2,29 @@ const { Sequelize } = require('sequelize');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+
+    const { v4: uuidv4 } = require('uuid');
+
     const users = [
       {
-        id: Sequelize.UUIDV4,
-        nome: 'John Doe',
+        id: uuidv4(),
+        nome: 'Usuario 1',
         sobrenome: 'Smith',
-        email: 'john.doe@example.com',
-        senha: 'hashed_password', // Replace with a hashed password
-        permissao: 'estudante',
+        email: 'usuario1@email.com',
+        senha: '$2b$08$gi1ty7Hb2uH4HeJGtaEBReJC26ZTxm8x2iA5FKGIDlGNa6n86VJva', //123456
+        permissao: 'criador',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
-        id: Sequelize.UUIDV4,
-        nome: 'Jane Smith',
-        email: 'jane.smith@example.com',
-        senha: 'hashed_password', // Replace with a hashed password
-        permissao: 'criador',
-      },
-      // Add more user objects as needed
+        id: uuidv4(),
+        nome: 'Usuario 2',
+        email: 'usuario2@email.com',
+        senha: '$2b$08$ujP8WLG0YQ3AX306LMJBr..Es9kHUha4R3UO4vAUTaTTCj4fKTDTq', //123456
+        permissao: 'estudante',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
     ];
 
     await queryInterface.bulkInsert('usuarios', users);
